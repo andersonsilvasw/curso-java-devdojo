f5eee1e5e65ef9df80048031ee68eadd2692b42c
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-04-28 21:37:41 -0300

    Alteração de nome do estudante2

--- src/maratonajavadevdojo/javacore/Bintroducaometodos/test/CalculadoraTest02.java
+++ src/maratonajavadevdojo/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -5,6 +5,6 @@ import maratonajavadevdojo.javacore.Bintroducaometodos.dominio.Calculadora;
 public class CalculadoraTest02 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.multiplicaDoisNumeros(10, 20);
+        calculadora.multiplicaDoisNumeros(10, 20.5F);
     }
 }
