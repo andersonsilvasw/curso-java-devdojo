a47db7cda6dad4ef513f94d7603f231fa2ed668b
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-05-04 14:04:56 -0300

    Aula de construtores com sobrecarga

--- src/maratonajavadevdojo/javacore/Dcontrutores/test/AnimeTest01.java
+++ src/maratonajavadevdojo/javacore/Dcontrutores/test/AnimeTest01.java
@@ -4,9 +4,7 @@ import maratonajavadevdojo.javacore.Dcontrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Hajime no Ippo", "TV", 350, "Artes marciais");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Hajime no Ippo", "TV", 350, "Artes marciais", "Production IG");
         anime.imprime();
-        anime2.imprime();
     }
 }
