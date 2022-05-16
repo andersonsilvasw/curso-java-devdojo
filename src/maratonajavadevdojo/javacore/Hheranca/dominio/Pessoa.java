67e379a414529afd0343a176fb4b5ba51bb83b1b
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-05-16 16:22:22 -0300

    Aula de Herança PT5 - Sequência de inicialização

--- src/maratonajavadevdojo/javacore/Hheranca/dominio/Pessoa.java
+++ src/maratonajavadevdojo/javacore/Hheranca/dominio/Pessoa.java
@@ -4,14 +4,19 @@ public class Pessoa {
     protected String nome;
     protected String cpf;
     protected Endereço endereço;
-
-    public Pessoa(String nome) {
-        this.nome = nome;
+    static {
+        System.out.println("Dentro do bloco de inicialização estático de pessoa");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de pessoa 1");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de pessoa 2");
     }
 
-    public Pessoa(String nome, String cpf) {
-        this(nome);
-        this.cpf = cpf;
+    public Pessoa(String nome) {
+        System.out.println("Dentro do construtor de pessoa");
+        this.nome = nome;
     }
 
     public void imprime(){
