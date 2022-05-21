2ef302de7e9ad4f047ca2fc70075bcfcd53c2150
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-05-21 10:40:10 -0300

    Aula 3 de Enumeração (Sobrescrita de métodos)

--- src/maratonajavadevdojo/javacore/Kenum/test/ClienteTest01.java
+++ src/maratonajavadevdojo/javacore/Kenum/test/ClienteTest01.java
@@ -2,13 +2,16 @@ package maratonajavadevdojo.javacore.Kenum.test;
 
 import maratonajavadevdojo.javacore.Kenum.dominio.Cliente;
 import maratonajavadevdojo.javacore.Kenum.dominio.TipoCliente;
+import maratonajavadevdojo.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Akira", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Ippo", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Akira", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Ippo", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
