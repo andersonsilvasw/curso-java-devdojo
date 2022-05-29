92fb980a60c8036825731c44fee97fe2fd1986b1
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-05-29 11:49:11 -0300

    Aula 02 Runtimexception

--- src/maratonajavadevdojo/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/maratonajavadevdojo/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -4,7 +4,7 @@ public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         // checked e unchecked
         int[] nums = {1,2};
-        // System.out.println(nums[2]);
-        // Comentei a linha acima para a exception não ser lançada
+        System.out.println(nums[2]);
+        // RuntimeException é erro do programador
     }
 }
