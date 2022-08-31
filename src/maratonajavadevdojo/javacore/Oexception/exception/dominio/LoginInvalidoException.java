e42c23d62c976df37da34d06f35366d8ead9c302
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-08-31 14:48:04 -0300

    Exceção customizada

---
+++ src/maratonajavadevdojo/javacore/Oexception/exception/dominio/LoginInvalidoException.java
@@ -0,0 +1,12 @@
+package maratonajavadevdojo.javacore.Oexception.exception.dominio;
+
+public class LoginInvalidoException extends Exception {
+
+    public LoginInvalidoException() {
+        super ("Login Inválido");
+    }
+
+    public LoginInvalidoException(String message) {
+        super(message);
+    }
+}
