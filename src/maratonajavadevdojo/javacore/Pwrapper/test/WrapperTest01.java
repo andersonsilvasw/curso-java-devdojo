1482d2e57123f4752ac5877def1be3e0704976fa
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-09-04 23:13:55 -0300

    Wrappers aula 2

--- src/maratonajavadevdojo/javacore/Pwrapper/test/WrapperTest01.java
+++ src/maratonajavadevdojo/javacore/Pwrapper/test/WrapperTest01.java
@@ -16,11 +16,23 @@ public class WrapperTest01 {
         // Classes wrappers
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; // autoboxing (Vai de um tipo primitivo para Wrapper)
         Long longW = 10L;
         Float floatW = 10F;
-        Double doubleW = 100;
+        Double doubleW = 10D;
         Character charW = 'W';
         Boolean bolleanW = false;
+
+        int i = intW; // unboxing (De wrapper para Tipo primitivo)
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("truE");
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
