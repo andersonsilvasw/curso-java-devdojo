d911495ce74a8a4ee881f66a2c18a9d79d89437d
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-10-20 17:43:13 -0300

    Aumento de salário

---
+++ src/beecrowd/Main1048.java
@@ -0,0 +1,37 @@
+package beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main1048 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double salario = sc.nextDouble();
+
+        if (salario >= 0 && salario <= 400.00) {
+            System.out.printf("Novo salario: %.2f\n", salario * 1.15);
+            System.out.printf("Reajuste ganho: %.2f\n", salario * 0.15);
+            System.out.println("Em percentual: 15 %");
+        } else if (salario >= 400.01 && salario <= 800.00) {
+            System.out.printf("Novo salario: %.2f\n", salario * 1.12);
+            System.out.printf("Reajuste ganho: %.2f\n", salario * 0.12);
+            System.out.println("Em percentual: 12 %");
+        } else if (salario >= 800.01 && salario <= 1200.00) {
+            System.out.printf("Novo salario: %.2f\n", salario * 1.10);
+            System.out.printf("Reajuste ganho: %.2f\n", salario * 0.10);
+            System.out.println("Em percentual: 10 %");
+        } else if (salario >= 1200.01 && salario <= 2000.00) {
+            System.out.printf("Novo salario: %.2f\n", salario * 1.07);
+            System.out.printf("Reajuste ganho: %.2f\n", salario * 0.07);
+            System.out.println("Em percentual: 7 %");
+        } else {
+            System.out.printf("Novo salario: %.2f\n", salario * 1.04);
+            System.out.printf("Reajuste ganho: %.2f\n", salario * 0.04);
+            System.out.println("Em percentual: 4 %");
+        }
+            sc.close();
+    }
+}
