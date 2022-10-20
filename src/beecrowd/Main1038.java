a74a164f0195bb97e7d2e582edc2097f08d503c2
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-10-19 21:25:21 -0300

    cardápio com switch case

---
+++ src/beecrowd/Main1038.java
@@ -0,0 +1,40 @@
+package beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main1038 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+        double total;
+
+        switch (codigo) {
+            case 1:
+                total = quantidade * 4.00;
+                System.out.printf("Total: R$ %.2f", total);
+                break;
+            case 2:
+                total = quantidade * 4.50;
+                System.out.printf("Total: R$ %.2f", total);
+                break;
+            case 3:
+                total = quantidade * 5.00;
+                System.out.printf("Total: R$ %.2f", total);
+                break;
+            case 4:
+                total = quantidade * 2.00;
+                System.out.printf("Total: R$ %.2f", total);
+                break;
+            case 5:
+                total = quantidade * 1.50;
+                System.out.printf("Total: R$ %.2f", total);
+                break;
+        }
+        sc.close();
+    }
+}
